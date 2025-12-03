f5990ea7f0daa7cc86bede7aff2a1855e1f4b390
YING YIFAN <yingyifan32@gmail.com>
2025-12-03 15:41:56 +0900

    feat: silence trimming for audio(close #15)

--- dataproc/audio/audio_proc.py
+++ dataproc/audio/audio_proc.py
@@ -13,3 +13,6 @@ def load_audio(path, sr=16000):
 def normalize_audio(y):
     return librosa.util.normalize(y)
 
+def trim_silence(y, top_db=20):
+    trimmed, _ = librosa.effects.trim(y, top_db=top_db)
+    return trimmed

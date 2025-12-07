fdba02329a1cb3a1f8a0a69d375983d8801c222a
Jdz1128 <3135103609@qq.com>
2025-12-07 14:33:08 +0900

    feat(video): update resize

--- video/video_tools.py
+++ video/video_tools.py
@@ -10,3 +10,60 @@ def cut_clip(input_path, output_path, start_time, end_time):
         ffmpeg_params=["-pix_fmt", "yuv420p"],
     )
     clip.close()
+
+# 缩放视频  resize
+def resize_video(input_path, output_path, scale=0.5):
+    clip = VideoFileClip(input_path)
+
+    orig_w, orig_h = clip.w, clip.h
+    new_w = int(orig_w * scale)
+    new_h = int(orig_h * scale)
+
+
+    if new_w % 2 == 1:
+        new_w -= 1
+    if new_h % 2 == 1:
+        new_h -= 1
+
+    new_clip = clip.resized(new_size=(new_w, new_h))
+
+
+    new_clip.write_videofile(
+        output_path,
+        codec="libx264",
+        audio_codec="aac",
+        ffmpeg_params=["-pix_fmt", "yuv420p"],
+    )
+
+    clip.close()
+    new_clip.close()
+
+def resize_to_resolution(input_path, output_path, target_w, target_h):
+    clip = VideoFileClip(input_path)
+
+    orig_w, orig_h = clip.w, clip.h
+    scale_w = target_w / orig_w
+    scale_h = target_h / orig_h
+    scale = min(scale_w, scale_h)
+
+    new_w = int(orig_w * scale)
+    new_h = int(orig_h * scale)
+
+
+    if new_w % 2 == 1:
+        new_w -= 1
+    if new_h % 2 == 1:
+        new_h -= 1
+
+    new_clip = clip.resized(new_size=(new_w, new_h))
+
+
+    new_clip.write_videofile(
+        output_path,
+        codec="libx264",
+        audio_codec="aac",
+        ffmpeg_params=["-pix_fmt", "yuv420p"],
+    )
+
+    clip.close()
+    new_clip.close()
